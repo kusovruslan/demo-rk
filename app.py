dedbcb5301a466d3c3d57da1b5354821e5801f87
Ruslan Kusov <kusovruslan@gmail.com>
2018-05-30 18:56:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ PORT_DEMO = 9090
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT_DEMO), Handler) as httpd:
-    print("Hello world! Listening at port", PORT_DEMO)
-    httpd.serve_forever()
+httpd = socketserver.TCPServer(("", PORT_DEMO), Handler)
+print("Hello world! Listening at port", PORT_DEMO)
+httpd.serve_forever()
