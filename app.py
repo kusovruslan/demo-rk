c8dae879bde35a4ca85a0e6f4167b03ac6e99162
Ruslan Kusov <kusovruslan@gmail.com>
2018-06-04 13:17:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import http.server
 import socketserver
+from colorama import Fore, Back, Style
 
 PORT_DEMO = 9090
 
 Handler = http.server.SimpleHTTPRequestHandler
 
 httpd = socketserver.TCPServer(("", PORT_DEMO), Handler)
-print("Hello world! Listening at port", PORT_DEMO)
+print(Fore.RED + "Hello world! Listening at port", PORT_DEMO)
 httpd.serve_forever()
